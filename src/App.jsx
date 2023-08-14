132394d7708d1b24da462746743b6498ff9a3ec0
TatyanaMolod <113109219+TatyanaMolod@users.noreply.github.com>
2023-08-14 12:49:25 +0600

    react_router

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header';
 import Counter from './components/Counter/Counter';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
  return(
-  <div>
-    <div className="wrapper">
-    <Header />
-    <Counter />
-    </div>
-  </div>
+  <BrowserRouter>
+      <div className="wrapper">
+        <Routes>
+          <Route path='*' element={<Header />} />
+        </Routes>
+        <main>
+        <Routes>
+            <Route path='/counter' element={<Counter />}/>
+        </Routes>
+      </main>
+      </div>
+  </BrowserRouter>
  );
 }
 
